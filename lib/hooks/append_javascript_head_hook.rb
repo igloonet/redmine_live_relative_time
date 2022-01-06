1eff07afed9d892c039c65c849cae365cfc5e8e2
Jethro Yu <comet.jc@gmail.com>
2022-01-07 04:00:25 +0800

    remove ru.js

--- lib/hooks/append_javascript_head_hook.rb
+++ lib/hooks/append_javascript_head_hook.rb
@@ -5,11 +5,6 @@ module RedmineLiveRelativeTime
         return javascript_include_tag('moment.min.js', :plugin => 'redmine_live_relative_time')
       end
     end
-    class AppendRUMomentLocaleJavascriptHeadHook < Redmine::Hook::ViewListener
-      def view_layouts_base_html_head(context={})
-        return javascript_include_tag('ru.js', :plugin => 'redmine_live_relative_time')
-      end
-    end
     class AppendLiveStampJavascriptHeadHook < Redmine::Hook::ViewListener
       def view_layouts_base_html_head(context={})
         return javascript_include_tag('livestamp.js', :plugin => 'redmine_live_relative_time')
